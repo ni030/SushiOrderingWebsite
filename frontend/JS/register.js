59beb7b8d0abf9b63c8e83c9e16e3354d9b63ab7
feizhen18 <147374055+feizhen18@users.noreply.github.com>
2024-06-18 02:20:40 +0800

    Add files via upload

--- frontend/JS/register.js
+++ frontend/JS/register.js
@@ -1,15 +1,45 @@
+const curPage = window.location.href;
+
+if(curPage === "http://localhost/SushiOrderingWebsite-main/frontend/register.php?singup=email"){
+    document.getElementById('email_error').style.display='block';
+}
+else{
+    document.getElementById('email_error').style.display='none';
+}
+if(curPage === "http://localhost/SushiOrderingWebsite-main/frontend/register.php?singup=phone"){
+    document.getElementById('phone_error').style.display='block';
+}
+else{
+    document.getElementById('phone_error').style.display='none';
+}
+
 document.getElementById('register-form').addEventListener('submit', function(event) {
     const password = document.getElementsByName('password')[0].value; // Get the value of the first element with name 'password'
     const confirmPassword = document.getElementsByName('confirmedPassword')[0].value; // Get the value of the first element with name 'confirmedPassword'
-    const errorMessage = document.getElementById('error-message');
+    var lowerCaseLetters=/[a-z]/g;
+    var upperCaseLetters=/[A-Z]/g;
+    var numbers=/[0-9]/g;
+    console.log("parent is called");
+
+    if(!(password.length>=8 && password.match(lowerCaseLetters)&&password.match(upperCaseLetters)&&password.match(numbers))){
+        console.log("enter if condition");
+        event.preventDefault(); // Prevent form submission
+        document.getElementById('password_error').style.display = 'block';
+        document.getElementById('email_error').style.display='none';
+        document.getElementById('phone_error').style.display='none';
+        document.getElementsByName('password')[0].value = ''; // Clear password field
+        document.getElementsByName('confirmedPassword')[0].value = ''; // Clear confirm password field
+        return;
+    }
 
     if (password !== confirmPassword) {
         event.preventDefault(); // Prevent form submission
-        errorMessage.style.display = 'block';
+        document.getElementById('matched_error').style.display = 'block';
+        document.getElementById('email_error').style.display='none';
+        document.getElementById('phone_error').style.display='none';
         document.getElementsByName('password')[0].value = ''; // Clear password field
         document.getElementsByName('confirmedPassword')[0].value = ''; // Clear confirm password field
     }
 });
-
 
 
