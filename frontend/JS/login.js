22167aedfba32ab2da448307a202625c71d9edde
feizhen18 <147374055+feizhen18@users.noreply.github.com>
2024-06-18 15:53:59 +0800

    Add files via upload

--- frontend/JS/login.js
+++ frontend/JS/login.js
@@ -2,6 +2,7 @@ const fullUrl=window.location.href;
 
 if(fullUrl.includes("login.php?signup=success")){
     alert("Your registration is suceessful!")
+    window.location.href="../frontend/login.php";
 }
 if(fullUrl.includes("login.php?signup=email")){
     document.getElementById('email_error').style.display='block';
