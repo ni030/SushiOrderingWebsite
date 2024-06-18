4d408ca7dff2a716db0274539f00e7da08b896ab
feizhen18 <147374055+feizhen18@users.noreply.github.com>
2024-06-18 12:41:40 +0800

    fix js

--- frontend/login.php
+++ frontend/login.php
@@ -1,22 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?php
-session_start();
-if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-    header("location: ../frontend/index.php");
-}
-?>
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/login.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-        </script>
+    </script>
+    <link rel="stylesheet" href="CSS/login.css">
     <title>Login</title>
 </head>
 
@@ -24,24 +17,23 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
     <nav class="navbar">
         <div class="navbar-left">
             <div class="navbar-brand">
-                <a href="#" class="navbar-logo">
-                    <img src="../img/logo.PNG" alt="Logo">
+                <a href="index.php" class="navbar-logo">
+                    <img src="img/logo.PNG" alt="Logo">
                 </a>
                 <span class="shop-name">Sushi Bliss</span>
             </div>
             <ul class="navbar-menu">
                 <li><a href="index.php" id="home-link">Home</a></li>
-                <li><a href="menu.html" id="menu-link">Menu</a></li>
-                <li><a href="promotion.html" id="promotion-link">Promotion</a></li>
+                <li><a href="menuPage.php" id="menu-link">Menu</a></li>
+                <li><a href="promotion.php" id="promo-link">Promotion</a></li>
             </ul>
         </div>
         <div class="navbar-profile">
             <a href="login.php" id="login-link">Login</a>
-            <span id="profile-name" style="display: none;"></span>
         </div>
     </nav>
     <div class="content">
-        <h2 style="color: white;">Log in now to enjoy exclusive deals and discounts!</h2>
+        <h2>Log in now to enjoy exclusive deals and discounts!</h2>
         <div class="form-container">
             <form id="login-form" method="POST" action="../backend/loginUser.php">
                 <div class="mb-3">
@@ -55,17 +47,18 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
                     <input type="password" id="inputPassword5" class="form-control" name="password" required>
                 </div>
                 <div class="col-12">
-                    <input type="submit" name="MM_select" value="Login">
+                    <p id="email_error" style="color: red; display: none;">Email is not existed!</p>
+                    <p id="pass_error" style="color: red; display: none;">Invalid password!</p>
                 </div>
                 <div class="col-12">
-                    <p id="error-message" style="color: red; display: none;"></p>
+                    <input type="submit" name="MM_select" value="Login">
                 </div>
             </form>
-            <p class="signup-link">Don't have an account? <a href="register.html">Sign up now!</a></p>
+            <p class="signup-link">Don't have an account? <a href="register.php">Sign up now!</a></p>
         </div>
     </div>
-    <script src="nav.js"></script>
-    <!-- <script src="login.js"></script> -->
+    <script src="JS/nav.js"></script>
+    <script src="JS/login.js"></script>
 </body>
 
 </html>
--- frontend/register.php
+++ frontend/register.php
@@ -4,8 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
     <link rel="stylesheet" href="CSS/register.css">
     <link rel="stylesheet" href="CSS/nav.css">
@@ -23,8 +25,8 @@
             </div>
             <ul class="navbar-menu">
                 <li><a href="index.php" id="home-link">Home</a></li>
-                <li><a href="menu.html" id="menu-link">Menu</a></li>
-                <li><a href="promotion.html" id="promotion-link">Promotion</a></li>
+                <li><a href="menuPage.php" id="menu-link">Menu</a></li>
+                <li><a href="promotion.php" id="promo-link">Promotion</a></li>
             </ul>
         </div>
         <div class="navbar-profile">
@@ -49,12 +51,14 @@
 
                 <div class="col-md-12">
                     <label for="inputEmail4" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="inputEmail4" name="email" placeholder="abc@gmail.com" required>
+                    <input type="email" class="form-control" id="inputEmail4" name="email" placeholder="abc@gmail.com"
+                        required>
                 </div>
 
                 <div class="col-md-12">
                     <label for="validationDefault02" class="form-label">Mobile Number</label>
-                    <input type="text" class="form-control" id="validationDefault02" name="mobileNum" placeholder="012-3456789" required>
+                    <input type="text" class="form-control" id="validationDefault02" name="mobileNum"
+                        placeholder="012-3456789" required>
                 </div>
 
                 <div class="col-md-12">
