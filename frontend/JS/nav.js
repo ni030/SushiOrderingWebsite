2f23a6454f3c11f15333dbd007383e9034a47ae7
feizhen18 <147374055+feizhen18@users.noreply.github.com>
2024-06-19 09:39:58 +0800

    Add active link for current page
    forget to upload sorry

--- frontend/JS/nav.js
+++ frontend/JS/nav.js
@@ -1,14 +1,15 @@
 const currentPage = window.location.pathname;
-console.log(currentPage);
 
-if(currentPage === "/SushiOrderingWebsite-main/frontend/index.php"){
+if(currentPage.includes("frontend/index.php")){
     document.getElementById('home-link').classList.add('active');
 }
-if(currentPage === "/SushiOrderingWebsite-main/frontend/menuPage.html"){
+else if(currentPage.includes("frontend/menuPage.php")){
     document.getElementById('menu-link').classList.add('active');
 }
-if(currentPage === "/SushiOrderingWebsite-main/frontend/promotion.html"){
+else if(currentPage.includes("frontend/promotion.html")){
     document.getElementById('promo-link').classList.add('active');
 }
-
+else if(currentPage==="/SushiOrderingWebsite-main/frontend/"){
+    document.getElementById('home-link').classList.add('active');
+}
 
