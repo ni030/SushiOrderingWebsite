761a6e1077de475f623b82f75ed10c9860bcdb27
LIM SI NI <130362958+ni030@users.noreply.github.com>
2024-06-22 22:05:11 +0800

    add sorting function

--- frontend/orderHistory.php
+++ frontend/orderHistory.php
@@ -4,9 +4,12 @@ require_once("../backend/connection.php");
 session_start();
 $uid = $_SESSION["id"];
 
-$sql = "SELECT * from orders WHERE orderUser = '$uid'";
+$sql = "SELECT * from orders WHERE orderUser = '$uid' ORDER BY orderTime DESC";
 $result = mysqli_query($conn, $sql);
-
+$orders = [];
+while ($row = $result->fetch_assoc()) {
+    $orders[] = $row;
+}
 
 ?>
 
@@ -16,7 +19,7 @@ $result = mysqli_query($conn, $sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>order History</title>
+    <title>Order History</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -28,6 +31,7 @@ $result = mysqli_query($conn, $sql);
     <link rel="stylesheet" href="CSS/order.css" />
     <script defer src="JS/nav.js"></script>
     <script defer src="JS/side-bar.js"></script>
+    <script defer src="JS/orderHistory.js"></script>
 </head>
 
 <body>
@@ -55,51 +59,44 @@ $result = mysqli_query($conn, $sql);
                             <input type="radio" name="sortOption" id="sortOldest" value="oldest">
                             <label for="sortOldest">Oldest to Newest</label>
                         </li>
-                        <li>
-                            <button class="btn btn-primary" id="applySort">Apply</button>
-                        </li>
                     </ul>
                 </div>
             </div>
-            <div class="order-list">
-                <?php
-                while ($row = $result->fetch_assoc()) {
-                ?>
+            <div class="order-list" id="orderList">
+                <?php foreach ($orders as $row) { ?>
                 <div class="order">
                     <div class="order-details">
                         <div class="order-time"><?php echo substr($row['ordertime'], 0, 19); ?></div>
                         <div class="order-item"><?php
-                                                    $mealArray = explode(',', $row["orderItems"]);
-                                                    $quantityArray = explode(',', $row["orderQuantity"]);
+                            $mealArray = explode(',', $row["orderItems"]);
+                            $quantityArray = explode(',', $row["orderQuantity"]);
 
-                                                    for ($x = 0; $x < count($mealArray); $x++) {
-                                                        $mealID = $mealArray[$x];
-                                                        $sqlMeal = "SELECT * FROM meal WHERE mealID = '$mealArray[$x]'";
-                                                        $resultM = mysqli_query($conn, $sqlMeal);
-                                                        $rowM = $resultM->fetch_assoc();
-                                                        echo "<p>" . $rowM["mealName"] . " x " . $quantityArray[$x] . "</p>";
-                                                    }
-
-                                                    ?></div>
+                            for ($x = 0; $x < count($mealArray); $x++) {
+                                $mealID = $mealArray[$x];
+                                $sqlMeal = "SELECT * FROM meal WHERE mealID = '$mealArray[$x]'";
+                                $resultM = mysqli_query($conn, $sqlMeal);
+                                $rowM = $resultM->fetch_assoc();
+                                echo "<p>" . $rowM["mealName"] . " x " . $quantityArray[$x] . "</p>";
+                            }
+                        ?></div>
                         <div class="order-address"><?php
-                                                        $curAddID = $row['addressID'];
-                                                        $sql2 = "SELECT * FROM address WHERE addressID = '$curAddID'";
-                                                        $result2 = mysqli_query($conn, $sql2);
-                                                        $row2 = $result2->fetch_assoc();
-                                                        echo $row2["addressName"] . " - ";
-                                                        echo $row2["unit"] . ", " . $row2["address"] . " " . $row2["city"] . ", " . $row2["postcode"] . ", " . $row2["state"];
-                                                        ?></div>
+                            $curAddID = $row['addressID'];
+                            $sql2 = "SELECT * FROM address WHERE addressID = '$curAddID'";
+                            $result2 = mysqli_query($conn, $sql2);
+                            $row2 = $result2->fetch_assoc();
+                            echo $row2["addressName"] . " - ";
+                            echo $row2["unit"] . ", " . $row2["address"] . " " . $row2["city"] . ", " . $row2["postcode"] . ", " . $row2["state"];
+                        ?></div>
                     </div>
                     <div class="order-column">
                         <div class="order-amount"><?php echo "RM " . $row['totalAmount']; ?></div>
                     </div>
                 </div>
-                <?php
-                }
-                ?>
+                <?php } ?>
             </div>
         </div>
     </div>
 </body>
+
 
 </html>
