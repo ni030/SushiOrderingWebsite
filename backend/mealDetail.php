68e3c41b4d10433b36e41f27bc9c9e2337a82c4d
LIM SI NI <130362958+ni030@users.noreply.github.com>
2024-06-22 19:35:15 +0800

    mealdetail php

--- backend/mealDetail.php
+++ backend/mealDetail.php
@@ -1,25 +1,16 @@
 <?php
-    require_once("connection.php");
-    $recordID = $_GET["meal"];
+require_once ("connection.php");
+$recordID = $_GET["meal"];
 
-    $sql = "SELECT * FROM meal WHERE mealID = '$recordID'";
-    $result = mysqli_query($conn, $sql);
-    $row = $result->fetch_assoc();
-
-?>
-
-<?php
-    session_start();
-
-    require_once("../backend/connection.php");
-
-    $sql = "SELECT * FROM meal WHERE category = 'Promotion'";
-    $result = mysqli_query($conn, $sql);
+$sql = "SELECT * FROM meal WHERE mealID = '$recordID'";
+$result = mysqli_query($conn, $sql);
+$row = $result->fetch_assoc();
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -29,63 +20,65 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 
     <!-- CSS links -->
+    <link rel="stylesheet" href="../frontend/CSS/style.css">
     <link rel="stylesheet" href="../frontend/CSS/mealDetail.css">
     <link rel="stylesheet" href="../frontend/CSS/nav.css">
-    <link rel="stylesheet" href="../frontend/CSS/style.css">
 
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    
+
 </head>
 
 <body>
     <!--nav bar-->
     <?php
-    include("../frontend/nav.php");
+    include ("../frontend/nav.php");
     ?>
 
-    <div class="pagination">
-        <p> Sushi details </p>
-    </div>
-    
     <!-- product section -->
-<div class="meal-container">
-    <div class="box" style="background-image: url('../frontend/img/detailBackground.png');">
-        <!-- Left side: Meal Image -->
-        <div class="meal-card">
-            <img src="../frontend/img/<?php echo $row["mealPic"]?>" alt="Meal Image">
-        </div>
-        <!-- Right side: Meal Details -->
-        <div class="meal-info">
-            <h3><?php echo $row["mealName"]; ?></h3>
-            <h3 class="price"><?php echo "each for RM " . $row["price"]; ?></h3>
-
-            <form method="POST" action="addCart.php">
-                <div class="quantity">
-                    <span>Quantity: </span>
-                    <div class="counter">
-                        <button class="minus" onclick="changeQuantity('minus'); return false;" type="button">-</button>
-                        <input type="text" class="" name="quantity" id="quantity" value="1">
-                        <button class="plus" onclick="changeQuantity('plus'); return false;" type="button">+</button>
-                    </div>
+    <div class="meal-container">
+        <div class="box" style="background-image: url('../frontend/img/detailBackground.png');">
+            <!-- Left side: Meal Image -->
+            <div class="meal-card">
+                <img src="../frontend/img/<?php echo $row["mealPic"] ?>" alt="Meal Image">
+            </div>
+            <!-- Right side: Meal Details -->
+            <div class="meal-info">
+                <div class="pagination"> Meal details </div>
+                <p><?php echo $row["mealName"]; ?></p>
+                <div class="mealType">
+                    <?php
+                    if ($row["category"] == "Promotion") {
+                        echo "<p style=\"color:#DC143C;\">&#129395 Hurry!! In PROMOTION! </p>";
+                    } else {
+                        echo "<p> Category:" . $row["category"] . "</p>";
+                    }
+                    ?>
                 </div>
-                <input type="hidden" name="mealID" value="<?php echo $row['mealID']; ?>">
-                <input type="submit" name="MM_update" value="Add to cart">
-            </form>
+                <p class="price"><?php echo "each for RM " . $row["price"]; ?></p>
+
+                <form method="POST" action="addCart.php">
+                    <div class="quantity">
+                        <span>Quantity: </span>
+                        <div class="counter">
+                            <button class="minus" onclick="changeQuantity('minus')" type="button">-</button>
+                            <input type="text" class="" name="quantity" id="quantity" value="1">
+                            <button class="plus" onclick="changeQuantity('plus')" type="button">+</button>
+                        </div>
+                    </div>
+                    <input type="hidden" name="mealID" value="<?php echo $row['mealID']; ?>">
+                    <input type="submit" name="MM_update" value="Add to cart">
+                </form>
+            </div>
         </div>
     </div>
-</div>
 
 
     <!-- script tags -->
-    <script src="../frontend/js/cart.js">
-
-    </script>
-
-
-
-
+    <script src="../frontend/JS/cart.js"></script>
 
 </body>
+
 </html>
