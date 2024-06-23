559d1c74309de13da469565beee6da287c57086c
Kai Xuen <147362689+OngKaiXuen@users.noreply.github.com>
2024-06-23 08:04:19 +0800

    Update viewCart.php

--- frontend/viewCart.php
+++ frontend/viewCart.php
@@ -71,6 +71,16 @@ $formattedDateTime = date("Y-m-d H:i:s", $timestamp);
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <style>
+        body{
+            background-image: url('../frontend/img/cartBackgroundTrans.png'); 
+            background-size: cover; 
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-position: center; 
+        }
+    </style>
 </head>
 
 <body>
